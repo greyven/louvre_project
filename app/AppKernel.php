02116ccaacdcf201e7cb25fcf541b1159d29ee84
Greyven <greyven@gmail.com>
2018-07-10 13:47:33 +0200

    Ajout de dépendances
    StofDoctrineExtensionsBundle
    DoctrineFixturesBundle
    SensioGeneratorBundle
    monolog-bridge

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,17 +17,19 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
 
-            if ('dev' === $this->getEnvironment()) {
-                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-            }
+//            if ('dev' === $this->getEnvironment()) {
+//                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+//            }
         }
 
         return $bundles;
