3d0d0bdba4533579e4e9b326ef69ac2709e7e2c9
Greyven <greyven@gmail.com>
2018-07-30 15:57:15 +0200

    Suppression checkoutAction(), try/catch déplacé dans prepareAction()

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Matcher\RedirectableUrlMatcher;
 
 /**
  * This controller is used to simulate an order from a customer.
@@ -17,47 +18,36 @@ class OrderController extends Controller
     /**
      * @Route("/prepare", name="order_prepare")
      */
-    public function prepareAction()
+    public function prepareAction(Request $request)
     {
-        return $this->render('prepare.html.twig',
-            array('stripe_public_key' => $this->getParameter('stripe_public_key')));
-    }
-
-    /**
-     * @Route(
-     *     "/checkout",
-     *     name="order_checkout",
-     *     methods="POST"
-     * )
-     */
-    public function checkoutAction(Request $request)
-    {
-        \Stripe\Stripe::setApiKey($this->getParameter('stripe_secret_key'));
-
-        if($request->isMethod('POST'))
-        {
+        if ($request->isMethod('POST')) {
             // Get the credit card details submitted by the form
+            \Stripe\Stripe::setApiKey($this->getParameter('stripe_secret_key'));
             $token = $request->get('stripeToken');
 
             // Create a charge: this will charge the user's card
-            try
-            {
+            try {
                 $charge = \Stripe\Charge::create(array(
                     "amount" => 1500, // Amount in cents
                     "currency" => "eur",
                     "source" => $token,
                     "description" => "Paiement Stripe - Exemple"
                 ));
+                dump($charge);
                 $this->addFlash("success", "Paiement éffectué !");
                 return $this->redirectToRoute("order_prepare");
-            }
-            catch(\Stripe\Error\Card $e)
-            {
+            } catch (\Stripe\Error\Card $e) {
 
                 $this->addFlash("error", "Erreur, paiement non éffectué !");
                 return $this->redirectToRoute("order_prepare");
                 // The card has been declined
             }
         }
+
+
+        return $this->render('prepare.html.twig',
+            array('stripe_public_key' => $this->getParameter('stripe_public_key')));
     }
+
+
 }
