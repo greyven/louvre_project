201d6806cdd795afa159bc400624ac12c9ba7824
Greyven <greyven@gmail.com>
2018-08-01 12:51:25 +0200

    Best practises Forms

--- src/AppBundle/Controller/CommandController.php
+++ src/AppBundle/Controller/CommandController.php
@@ -20,26 +20,24 @@ class CommandController extends Controller
         $command = new Command();
         $commandForm = $this->createForm(CommandType::class, $command);
 
-        // Si on a des données 'POST' c'est que le formulaire a été envoyé donc on le traite
-        if($request->isMethod('POST'))
+
+        // Récupère les données du Form et hydrate $command
+        $commandForm->handleRequest($request);
+
+        // Si on a des données 'POST' et Valides, c'est que le formulaire a été envoyé donc on le traite
+        if($commandForm->isSubmitted() && $commandForm->isValid())
         {
-            // Récupère les données du Form et hydrate $command
-            $commandForm->handleRequest($request);
-
-            // Si les données saisies sont valides
-            if($commandForm->isValid())
+            // insertion des tickets vides
+            for($i = 0 ; $i < $command->getNumberOfTickets(); $i++ )
             {
-                // insertion des tickets vides
-                for($i = 0 ; $i < $command->getNumberOfTickets(); $i++ ){
-                    $command->addTicket(new Ticket());
-                }
-
-                // sauvegarde $command en variable session
-                $request->getSession()->set('command', $command);
-
-                // Redirection vers le second formulaire auquel on passe l'objet $command
-                return $this->redirectToRoute('app_tickets');
+                $command->addTicket(new Ticket());
             }
+
+            // sauvegarde $command en variable session
+            $request->getSession()->set('command', $command);
+
+            // Redirection vers le second formulaire auquel on passe l'objet $command
+            return $this->redirectToRoute('app_tickets');
         }
 
         // Soit le visiteur arrive sur la page, soit des données du formulaire ne sont pas valides
@@ -53,44 +51,35 @@ class CommandController extends Controller
     {
         $command = $request->getSession()->get('command');
         $grammar = $command->getNumberOfTickets() > 1 ? ' billets' : ' billet';
-        $halfDay = $command->getTicketType() ? ' demi-journée' : ' journée';
+        $halfDay = $command->getFullDay() ? ' demi-journée' : ' journée';
         $recap = 'Vous souhaitez réserver '.$command->getNumberOfTickets().$grammar.$halfDay.
         ' pour le '.$command->getVisitDate()->format('d-m-Y').'.';
 
         $ticketsCollection = $this->createForm(TicketFormCollectionType::class, $command);
 
-        // Si on a des données 'POST' c'est que le formulaire a été envoyé donc on le traite
-        if($request->isMethod('POST'))
+
+        // Récupère les données des forms et hydrate $command
+        $ticketsCollection->handleRequest($request);
+
+        // Si on a des données 'POST' et Valides, c'est que le formulaire a été envoyé donc on le traite
+        if($ticketsCollection->isSubmitted() && $ticketsCollection->isValid())
         {
-            // Récupère les données des forms et hydrate $command
-            $ticketsCollection->handleRequest($request);
+            $totalPrice = 0;
 
-            // Si les forms sont valides
-            if($ticketsCollection->isValid())
+            // On persist chaque $ticket dans la collection
+            foreach ($command->getTickets() as $ticket)
             {
-                $em = $this->getDoctrine()->getManager();
-
-                $totalPrice = 0;
-                $visitorsNames = '';
-
-                // On persist chaque $ticket dans la collection
-                foreach ($command->getTickets() as $ticket)
-                {
-                    $ticketPrice = $ticket->defineTicketCost();
-                    $totalPrice = $totalPrice + $ticketPrice;
-
-                    $ticketName = $ticket->getFirstName()." ".$ticket->getLastName();
-                    $visitorsNames = $visitorsNames."/".$ticketName;
-                }
-
-                // on complète les champs vides de la commande
-                $command->setTotalPrice($totalPrice);
-                $command->setReservationDate(new \DateTime());
-
-                $request->getSession()->set('command', $command);
-                // Redirection vers la page de paiement
-                return $this->redirectToRoute('app_bill');
+                $ticketPrice = $ticket->defineAndSetTicketPrice();
+                $totalPrice = $totalPrice + $ticketPrice;
             }
+
+            // on complète les champs vides de la commande
+            $command->setTotalPrice($totalPrice);
+            $command->setReservationDate(new \DateTime());
+
+            $request->getSession()->set('command', $command);
+            // Redirection vers la page de paiement
+            return $this->redirectToRoute('app_bill');
         }
 
         // Sinon le visiteur arrive sur la page, ou bien des données du formulaire ne sont pas valides
