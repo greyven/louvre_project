997ca2a170265a667d9bdaebce968b331a18523f
Greyven <greyven@gmail.com>
2018-08-12 14:46:51 +0200

    Ajout de tests fonctionnels

--- tests/AppBundle/Controller/LouvreControllerTest.php
+++ tests/AppBundle/Controller/LouvreControllerTest.php
@@ -18,4 +18,45 @@ class LouvreControllerTest extends WebTestCase
 
         echo $client->getResponse()->getContent();
     }
+
+    public function testCommandIsUp()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/command');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        echo $client->getResponse()->getContent();
+    }
+    
+
+    public function testContactIsUp()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/contact');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        echo $client->getResponse()->getContent();
+    }
+
+    public function testCommand()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/home');
+
+        $link = $crawler->selectLink('Billetterie')->link();
+        $crawler = $client->click($link);
+
+        $form = $crawler->selectButton('Valider')->form();
+        $form['Commande_:[fullDay]'] = 0;
+        $form['Commande_:[visitDate][day]'] = 13;
+        $form['Commande_:[visitDate][month]'] = 8;
+        $form['Commande_:[visitDate][year]'] = 2018;
+        $form['Commande_:[numberOfTickets]'] = 1;
+        $form['Commande_:[visitorEmail]'] = 'greyven@gmail.com';
+
+        $client->submit($form);
+        $client->followRedirect();
+    }
 }
