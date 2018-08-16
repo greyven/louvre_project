e90fe8c8c058973d7ce41ce9e589e2958c207527
Greyven <greyven@gmail.com>
2018-08-16 18:28:15 +0200

    création route/action/vue mailed

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -20,9 +20,27 @@ class ContactController extends Controller
 
         if ($contactForm->isSubmitted() && $contactForm->isValid())
         {
-            return $this->redirectToRoute('app_home');
+            $request->getSession()->set('mailed', true);
+            return $this->redirectToRoute('app_mailed');
         }
 
         return $this->render('contact.html.twig', array('contactForm' => $contactForm->createView()));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function mailedAction(Request $request)
+    {
+        $mailed = $request->getSession()->get('mailed');
+        $request->getSession()->remove('mailed');
+
+        if($mailed)
+        {
+            $this->addFlash('success', 'Votre message a bien été envoyé.');
+        }
+
+        return $this->render('mailed.html.twig');
     }
 }
