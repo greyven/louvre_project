dd797e2d1a79482575e42699b9a3364b2e2f6ec3
Greyven <greyven@gmail.com>
2018-08-16 11:33:17 +0200

    Class Service perso Mailer

--- src/AppBundle/Service/Mailer.php
+++ src/AppBundle/Service/Mailer.php
@@ -1,32 +1,56 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Windows
- * Date: 13/08/2018
- * Time: 10:51
- */
 
 namespace AppBundle\Service;
 
 
 use AppBundle\Entity\Command;
+use Swift_Message;
 
 class Mailer
 {
     /**
+     * @var \Swift_Mailer
+     */
+    private $swift_Mailer;
+    /**
      * @var \Twig_Environment
      */
     private $twig;
+    private $mailSender;
 
-    public function __construct(\Twig_Environment $twig)
+    /**
+     * Mailer constructor.
+     * @param \Swift_Mailer $swift_Mailer
+     * @param \Twig_Environment $twig
+     * @param $mailSender
+     */
+    public function __construct(\Swift_Mailer $swift_Mailer, \Twig_Environment $twig, $mailSender)
     {
         $this->twig = $twig;
+        $this->mailSender = $mailSender;
+        $this->swift_Mailer = $swift_Mailer;
     }
 
-
-    public function sendCommandConfirmation(Command $command)
+    /**
+     * @param Command $command
+     * @return Swift_Message
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function sendConfirmationMail(Command $command)
     {
-        $body = $this->twig->render('kop',['command'=>$command]);
+        $body = $this->twig->render('mail.command.html.twig',['command'=>$command]);
+
+        // Creer le message
+        $message = (new Swift_Message('Commande n°'.$command->getId()))
+            ->setFrom($this->mailSender)
+            ->setTo([$command->getVisitorEmail()])
+            ->setBody($body)
+        ;
+
+        // Envoyer le message
+        return $this->swift_Mailer->send($message);
     }
 
 }
