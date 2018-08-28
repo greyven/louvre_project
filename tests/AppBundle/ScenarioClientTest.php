d48c605f7187acbf0d84c99ceecde912cf9ece49
Greyven <greyven@gmail.com>
2018-08-28 15:52:02 +0200

    Move class

--- tests/AppBundle/ScenarioClientTest.php
+++ tests/AppBundle/ScenarioClientTest.php
@@ -68,37 +68,13 @@ class ScenarioClientTest extends WebTestCase
 //        COMMAND
         $formCommand = $crawler->selectButton('Valider')->form();
         $formCommand['command[fullDay]'] = 1;
-        $formCommand['command[visitDate][day]'] = 13;
+        $formCommand['command[visitDate][day]'] = 28;
         $formCommand['command[visitDate][month]'] = 8;
         $formCommand['command[visitDate][year]'] = 2018;
-        $formCommand['command[numberOfTickets]'] = 0;
+        $formCommand['command[numberOfTickets]'] = 1;
         $formCommand['command[visitorEmail]'] = 'greyven@gmail';
+        $client->submit($formCommand);
 
-        $this->expectException();
-//        $client->submit($formCommand);
-//        $this->assertTrue($client->getResponse()->isRedirection());
-//        $crawler = $client->followRedirect();
-//
-//        $this->assertContains('billet', $client->getResponse()->getContent());
-//        $this->assertContains('journée', $client->getResponse()->getContent());
-//        $this->assertContains('13/12/2018', $client->getResponse()->getContent());
-//
-////        TICKETS
-//        $formTicketsCollection = $crawler->selectButton('Valider')->form();
-//        $formTicketsCollection['ticket_form_collection[tickets][0][lastName]'] = 'Séré';
-//        $formTicketsCollection['ticket_form_collection[tickets][0][firstName]'] = 'Stef';
-//        $formTicketsCollection['ticket_form_collection[tickets][0][birthDate][day]'] = 4;
-//        $formTicketsCollection['ticket_form_collection[tickets][0][birthDate][month]'] = 11;
-//        $formTicketsCollection['ticket_form_collection[tickets][0][birthDate][year]'] = 1983;
-//        $formTicketsCollection['ticket_form_collection[tickets][0][country]'] = 'FR';
-//        //$formTicketsCollection['ticket_form_collection[tickets][0][reducedPrice]']->tick();
-//
-//        $client->submit($formTicketsCollection);
-//        $this->assertTrue($client->getResponse()->isRedirection());
-//        $crawler = $client->followRedirect();
-//
-//        $this->assertContains('16€', $client->getResponse()->getContent());
-//
-//        //$modal = $crawler->filter('Payer la commande')->form();
+        $this->assertTrue($client->getResponse()->isSuccessful());
     }
 }
